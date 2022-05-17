61277c765b9b1482088d44efbafccc59c931dbc6
Leo <leohernandesvasc@gmail.com>
2022-05-17 17:26:08 -0300

    Petianos e Ex-Petianos são mostrados em slides

--- integrantes/scripts.js
+++ integrantes/scripts.js
@@ -1,28 +1,56 @@
-var slideIndex = 1;
-showSlides(slideIndex);
+var activePetianosSlideIndex = 1;
+var exPetianosSlideIndex = 1;
+activePetianosShowSlides(activePetianosSlideIndex);
+exPetianosShowSlides(exPetianosSlideIndex);
 
 // Next/previous controls
-function plusSlides(n) {
-    showSlides(slideIndex += n);
+function activePetianosPlusSlides(n) {
+    activePetianosShowSlides(activePetianosSlideIndex += n);
+}
+
+function exPetianosPlusSlides(n){
+    exPetianosShowSlides(exPetianosSlideIndex += n)
 }
 
 // Thumbnail image controls
-function currentSlide(n) {
-    showSlides(slideIndex = n);
+function activePetianosCurrentSlide(n) {
+    activePetianosShowSlides(activePetianosSlideIndex = n);
 }
 
-function showSlides(n) {
+function exPetianosCurrentSlide(n) {
+    exPetianosShowSlides(exPetianosSlideIndex = n);
+}
+
+function activePetianosShowSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    var dots = document.getElementsByClassName("dot");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
+    var container = document.getElementById('active-petianos');
+    var slides = container.getElementsByClassName('mySlides');
+    var dots = container.getElementsByClassName("dot");
+    if (n > slides.length) { activePetianosSlideIndex = 1 }
+    if (n < 1) { activePetianosSlideIndex = slides.length }
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
     for (i = 0; i < dots.length; i++) {
         dots[i].className = dots[i].className.replace(" active", "");
     }
-    slides[slideIndex - 1].style.display = "flex";
-    dots[slideIndex - 1].className += " active";
+    slides[activePetianosSlideIndex - 1].style.display = "flex";
+    dots[activePetianosSlideIndex - 1].className += " active";
+}
+
+function exPetianosShowSlides(n) {
+    var i;
+    var container = document.getElementById('ex-petianos');
+    var slides = container.getElementsByClassName('mySlides');
+    var dots = container.getElementsByClassName("dot");
+    if (n > slides.length) { exPetianosSlideIndex = 1 }
+    if (n < 1) { exPetianosSlideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[exPetianosSlideIndex - 1].style.display = "flex";
+    dots[exPetianosSlideIndex - 1].className += " active";
 }
